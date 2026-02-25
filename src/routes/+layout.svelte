01098aa8c5e3f5b76a34ee1cb6fc0f1edd429daa
ZippyDaFish <andrew.riker21@gmail.com>
2026-02-24 20:12:46 -0500

    Hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,9 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<header class="absolute top-0 left-0 w-full p-8">
+	<div class="w-16 h-16 border-2 border-gray-400 rounded-full"></div>
+</header>
 {@render children()}
+<footer></footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
+<script>
+    import heroMedia from "$lib/assets/heroMedia.jpg";
+</script>
+
 <!-- Hero -->
 <section class="min-h-screen flex flex-col md:flex-row">
     <!-- Hero Text -->
     <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-surface)]">
         <div class="w-full max-w-xl px-6">
-            <h1>Precision Custom Tooling</h1>
-            <h3>Tagline</h3>
+            <h1 class="text-6xl mb-8">Precision Custom Tooling</h1>
+            <h3 class="text-lg">Exactly what you need (Within tolerance).</h3>
         </div>
     </div>
     <!-- Hero Media -->
-    <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-bg)]">
-        <div class="w-full max-w-xl px-6">
-            <img src="" alt="" />
+    <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-surface)] h-screen overflow-hidden">
+        <div class="w-full max-w-xl px-6 flex items-center justify-center h-full">
+            <img src={heroMedia} alt="" class="max-h-[80%] w-auto object-contain" />
         </div>
     </div>
 </section>
