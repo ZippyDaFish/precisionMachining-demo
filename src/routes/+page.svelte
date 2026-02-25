b192451d056d4feb71e996c7c8c94c7ea34abf7a
ZippyDaFish <andrew.riker21@gmail.com>
2026-02-24 19:45:14 -0500

    Add global styles and some layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- Hero -->
+<section class="min-h-screen flex flex-col md:flex-row">
+    <!-- Hero Text -->
+    <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-surface)]">
+        <div class="w-full max-w-xl px-6">
+            <h1>Precision Custom Tooling</h1>
+            <h3>Tagline</h3>
+        </div>
+    </div>
+    <!-- Hero Media -->
+    <div class="md:w-1/2 w-full flex items-center justify-center bg-[var(--color-bg)]">
+        <div class="w-full max-w-xl px-6">
+            <img src="" alt="" />
+        </div>
+    </div>
+</section>
+
+<!-- Trust -->
+<section class="w-full min-h-[50vh] flex items-center bg-[var(--color-surface)]">
+    <div class="w-full max-w-6xl mx-auto px-6">
+
+    </div>
+</section>
